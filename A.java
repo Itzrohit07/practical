74139238a5dd1fb4a289c2fe461bdb720750cd62
Itzrohit07 <110466806+Itzrohit07@users.noreply.github.com>
2022-08-09 18:17:14 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,19 @@
+import jdk.dynalink.beans.StaticClass;
+
+import java.util.Scanner;
+
+public class A {
+     static int c=30;
+    int a=20;
+    public static void main(String[] args) {
+        A obj=new A();
+        int b=10;
+        System.out.println(A.c);
+        System.out.println(b);
+        System.out.println(obj.a);
+
+
+
+
+    }
+}
