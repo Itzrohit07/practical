af90d5bc358cc6a327ec840693d26116ca2023a0
Itzrohit07 <110466806+Itzrohit07@users.noreply.github.com>
2022-08-09 18:17:36 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,11 @@
+public class Addition {
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+
+        System.out.printf("sum =%d\n",a+b);
+        System.out.printf("differance =%d\n", a-b);
+        System.out.printf("multiplication =%d\n",a*b);
+        System.out.printf("division =%d\n",a/b);
+    }
+}
