b60022b5f985d7753132c88a6c8619fc8a8e910f
Itzrohit07 <110466806+Itzrohit07@users.noreply.github.com>
2022-08-23 21:01:53 +0530

    Add files via upload

---
+++ Infinityforloop.java
@@ -0,0 +1,9 @@
+//use infinity forloop.
+import java.util.*;
+public class Infinityforloop {
+    public static void main(String[] args) {
+        for (; ; ) {
+            System.out.println("yes i can do it");
+        }
+    }
+}
