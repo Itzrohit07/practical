83cd79e7795082390a5aa441c880e7c819cde243
Itzrohit07 <110466806+Itzrohit07@users.noreply.github.com>
2022-08-23 20:58:10 +0530

    Add files via upload

---
+++ foreachloop2.java
@@ -0,0 +1,14 @@
+//use for each loop as a simple program and print the output.
+import java.util.*;
+public class foreachloop2 {
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5};
+
+        for (int i : arr) {
+
+            System.out.println(i);
+
+        }
+    }
+}
+
