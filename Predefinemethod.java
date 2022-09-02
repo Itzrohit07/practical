c7156e6f81d7665eedfb70e60cb2261f83914116
Itzrohit07 <110466806+Itzrohit07@users.noreply.github.com>
2022-09-02 11:06:29 +0530

    Add files via upload

---
+++ Predefinemethod.java
@@ -0,0 +1,10 @@
+package testprogram;
+
+public class Predefinemethod {
+
+	public static void main(String[] args) {
+		System.out.println("the maximum number is:"+Math.max(5, 9));
+        System.out.println("the minimum number is:"+Math.min(5, 9));
+	}
+
+}
