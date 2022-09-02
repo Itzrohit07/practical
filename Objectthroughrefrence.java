ae77e25e749d6423a782acbcb9d4961e07f4df9e
Itzrohit07 <110466806+Itzrohit07@users.noreply.github.com>
2022-09-02 11:03:23 +0530

    Add files via upload

---
+++ Objectthroughrefrence.java
@@ -0,0 +1,22 @@
+package testprogram;
+
+public class Objectthroughrefrence {
+
+	
+	
+		
+		int age;
+		String name;
+	
+	
+		public static void main(String[] args) {
+		Objectthroughrefrence tc=new Objectthroughrefrence();
+		tc.age=10;
+		tc.name="Rohit";
+		System.out.println("enter your age");
+		System.out.println("enter your name");
+		System.out.println(tc.age+" "+tc.name);
+	}
+	}
+
+
