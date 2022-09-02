d798ed8ec93209ed17064090f7bc3c66ad98aa46
Itzrohit07 <110466806+Itzrohit07@users.noreply.github.com>
2022-09-02 11:08:31 +0530

    Add files via upload

---
+++ Primenumber.java
@@ -0,0 +1,26 @@
+//write a java program to check a number prime or not.
+package testprogram;
+import java.util.*;
+public class Primenumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int temp,i,count=0;
+       Scanner sc=new Scanner(System.in);
+       System.out.println("enter a number");
+       temp=sc.nextInt();
+       for(i=2;i<temp; i++) {
+    	   if(temp%i==0) {
+    	   count++;
+    	   break;
+    	   }
+       }
+       if(count==0) {
+    	   System.out.println("it is a prime number");
+       }else {
+    	   System.out.println("it is not a prime number");
+    	   
+       }
+	}
+
+}
---
+++ Staticmethod.java
@@ -0,0 +1,13 @@
+package testprogram;
+
+public class Staticmethod {
+
+	public static void main(String[] args) {
+		display();
+	}
+     static void display() {
+	  System.out.println("welcome to coding world");
+}
+	}
+
+
