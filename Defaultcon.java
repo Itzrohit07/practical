159cfb8dc1e196a13d08bbfb8d21c60dd07472c7
Itzrohit07 <110466806+Itzrohit07@users.noreply.github.com>
2022-09-06 18:19:06 +0530

    Add files via upload

---
+++ Defaultcon.java
@@ -0,0 +1,12 @@
+//create a constructor.
+package testprogram;
+
+public class Defaultcon {
+     Defaultcon(){
+    	 System.out.println("constructor is Default");}
+     
+	public static void main(String[] args) {
+		Defaultcon a=new Defaultcon();
+	}
+
+}
